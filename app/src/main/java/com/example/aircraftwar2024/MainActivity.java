5a1fa03d881c4a70c5bf8751dca2a70c9633bb17
aetherqaq <2448301544@qq.com>
2024-05-30 14:28:40 +0800

    2.0

--- app/src/main/java/com/example/aircraftwar2024/MainActivity.java
+++ app/src/main/java/com/example/aircraftwar2024/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.aircraftwar2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.musicon);
+        Button btn2 = (Button) findViewById(R.id.musicoff);
+        Button btn3 = (Button) findViewById(R.id.start);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.musicoff){
+            Toast.makeText(this, "Music is off", Toast.LENGTH_SHORT).show();
+        } else if (v.getId() == R.id.musicon) {
+            Toast.makeText(this, "Music is on", Toast.LENGTH_SHORT).show();
+        } else if (v.getId() == R.id.start) {
+            Intent intent = new Intent(MainActivity.this, OfflineActivity.class);
+            intent.putExtra("user","test");
+            startActivity(intent);
+        }
+    }
+
+
+
+
 }
